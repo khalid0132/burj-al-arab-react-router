e82fcb3542b9092d8674c4ec9d9b9346ac020321
Khalid Saifullah <khalid0132@gmail.com>
2021-03-17 18:56:34 +0100

    PrivateRoute and Redirect Auth added

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAmwuVjIrb2be4zrloJdGAPOxfTB7yH7Q0",
+    authDomain: "burj-al-arab-0132.firebaseapp.com",
+    projectId: "burj-al-arab-0132",
+    storageBucket: "burj-al-arab-0132.appspot.com",
+    messagingSenderId: "351880631727",
+    appId: "1:351880631727:web:471341851caf8db25d8b5a"
+  };
+  export default firebaseConfig;
